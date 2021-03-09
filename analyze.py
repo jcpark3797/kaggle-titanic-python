8ca931733000483df4257f973648cb688d474f1d
jcpark3797 <jcpark3797@gmail.com>
2021-03-09 23:47:53 +0900

    commit version v1.1

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 
 
 
-def check_servived_table(train_data):
+def print_survived_info_table(train_data):
     print(train_data[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)) #t_pclass 티켓등급
     print('-'*20)
     print(train_data[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)) # t_sex 성별
--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import numpy as np
 import pandas as pd
 import os
 import seaborn as sns
+import matplotlib.pyplot as plt
 import analyze
 
+
 if __name__ == "__main__":
+    # Load Dataset
     links = ['./input/train.csv', './input/test.csv', './input/gender_submission.csv']
     print(links)
+
+    # Analyze Data
+    """
+        train_data: Cabin > Age > Embarked(승선항)에 null 값이 존재한다.
+        test_data: Cabin > Age > Fare(1건)에 null 값이 존재한다.
+    """
 
     print('-'*20)
     train_data = pd.read_csv(links[0])
     test_data = pd.read_csv(links[1])
     print('> check train data null values')
-    print(train_data.isnull().sum())
+    print(train_data.info())
+    #print(train_data.isnull().sum())
     print('> check test data null values')
-    print(test_data.isnull().sum())
+    print(test_data.info())
+    #print(test_data.isnull().sum())
+    print('-'*20)
+    print(train_data.describe())
+    print('-'*20)
+    print(train_data.describe(include=['O']))
     print('-'*20)
 
 
@@ -26,5 +41,21 @@ if __name__ == "__main__":
     print('name_dic =', name_dic)
     print('-'*20)
 
-    analyze.check_servived_table(train_data) # analyze.py
+    #analyze.print_survived_info_table(train_data) # analyze.py
+    """
+        age.png
+        본 그래프를 통해 아래와 같은 사실을 얻게 되었다.
+        1) 유아는 생존율 높음 (4이하)
+        2) 80세 생존
+        3) 15-35? 대부분 생존하지 못함
+    """
+    g = sns.FacetGrid(train_data, col='Survived')
+    g.map(plt.hist, 'Age', bins=20)
+    # g.savefig('test.png') or plt.show()
 
+    grid = sns.FacetGrid(train_data, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+    grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+    grid.add_legend()
+    # grid.savefig('Pclass.png') or plt.show()
+
+    # study https://www.kaggle.com/startupsci/titanic-data-science-solutions
